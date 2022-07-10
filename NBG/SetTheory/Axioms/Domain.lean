9a5ff9b9ca1acfe027441d121eef51bacf872194
furea2 <furea2@hotmail.co.jp>
2022-07-10 20:05:04 +0900

    modify axiom of domain

--- NBG/SetTheory/Axioms/Domain.lean
+++ NBG/SetTheory/Axioms/Domain.lean
@@ -6,22 +6,29 @@ open Classical
 -- 7. AxiomDomain
 axiom AxiomDomain :
   ∀X: Class, ∃D: Class,
-    ∀x: Class, ∀_: Set x,
-      (x∈D ↔ ∃y: Class, ∃_: Set y, (＜x, y＞ ∈ X))
+    ∀z: Class,
+      (z∈D ↔ (∃x: Class, ∃_: Set x, ∃y: Class, ∃_: Set y,
+        ∃_:＜x, y＞ ∈ X, (z ＝ x)))
 
 -- domain
 noncomputable def Dom (X: Class): Class :=
   choose (AxiomDomain X)
 noncomputable def Dom_def (X: Class):
-  ∀x: Class, ∀_: Set x,
-    (x∈(Dom X) ↔ ∃y: Class, ∃_: Set y, (＜x, y＞ ∈ X)) :=
+  ∀z: Class,
+    (z∈(Dom X) ↔ (∃x: Class, ∃_: Set x, ∃y: Class, ∃_: Set y,
+      ∃_:＜x, y＞ ∈ X, (z ＝ x))) :=
   choose_spec (AxiomDomain X)
 
 -- rangle
 noncomputable def Rng (X: Class) [Relation X]: Class :=
   choose (AxiomDomain (RelInv X))
-noncomputable def Rng_def (X: Class) [Relation X]:
-  ∀x: Class, ∀_: Set x, (x∈(Dom (RelInv X))
-    ↔ ∃y: Class, ∃_: Set y, (＜x, y＞ ∈ (RelInv X))) :=
+noncomputable def Rng_def' (X: Class) [Relation X]:
+  ∀z: Class,
+    (z∈(Dom (RelInv X)) ↔ (∃x: Class, ∃_: Set x, ∃y: Class, ∃_: Set y,
+      ∃_:＜x, y＞ ∈ (RelInv X), (z ＝ x))) :=
   choose_spec (AxiomDomain (RelInv X))
+noncomputable def Rng_def (X: Class) [Relation X]:
+  ∀z: Class,
+    (z∈(Dom X) ↔ (∃x: Class, ∃_: Set x, ∃y: Class, ∃_: Set y,
+      ∃_:＜x, y＞ ∈ X, (z ＝ y))) := sorry
 
